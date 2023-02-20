1457d2aaf7bc9704fc3708d0a870b8e8f62f4978
Haruka Nishida <nishida1436@gmail.com>
2023-02-20 19:04:40 +0900

    記事一覧の取得エラー解消

--- components/layouts/works/worksSection.tsx
+++ components/layouts/works/worksSection.tsx
@@ -2,8 +2,7 @@ import styles from '../../../styles/works.module.scss';
 import Image from "next/legacy/image";
 import Link from 'next/link';
 
-
-export default function Works() {
+export default function WorksSection(props: any) {
   return (
     <>
       <section className="container">
@@ -11,10 +10,17 @@ export default function Works() {
           <div className={styles.title}>
             <h2 className={styles.titleText}>works</h2>
           </div>
-
+          <ul>
+            {props.works.map((work: any) => (
+              <li key={work.id}>
+                <Link href={`/works/${work.id}`}>
+                  {work.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
     </>
   )
 }
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
 import type { Works } from '../types/works';
 import { client } from '../libs/client';
-import Top from '../components/layouts/top/topSection'
-import About from '../components/layouts/about/aboutSection'
+import TopSection from '../components/layouts/top/topSection'
+import AboutSection from '../components/layouts/about/aboutSection'
+import WorksSection from '../components/layouts/works/worksSection'
 
 type Props = {
   works: Array<Works>;
@@ -12,17 +13,9 @@ type Props = {
 export default function Home({ works }: Props) {
   return (
     <>
-      <Top />
-      <About />
-      <ul>
-        {works.map((works) => (
-          <li key={works.id}>
-            <Link href={`/works/${works.id}`}>
-              {works.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <TopSection />
+      <AboutSection />
+      <WorksSection works={ works }/>
     </>
   )
 }
