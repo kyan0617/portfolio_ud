a3da37426ca4123f656cd25664a52584d65b4b7f
Haruka Nishida <nishida1436@gmail.com>
2023-03-07 16:24:52 +0900

    worksセクションのレスポンシブ実装中

--- components/layouts/works/worksSection.tsx
+++ components/layouts/works/worksSection.tsx
@@ -14,37 +14,55 @@ export default function WorksSection(props: Props) {
   return (
     <>
       <section className="container">
-        <div className={styles.inner}>
-          <div className={styles.title}>
-            <h2 className={styles.titleText}>works</h2>
+        <div className={styles.wrapper}>
+          <div className={`${styles["inner"]} _inner`}>
+            <div className={styles.title}>
+              <h2 className={styles.titleText}>works</h2>
+            </div>
+            <Splide
+              area-label="Works"
+              options={{
+                speed: 1500,
+                pagination: false,
+                type: 'loop',
+                lazyLoad: 'nearby',
+                preloadPages: 2,
+                rewind: true,
+                perMove: 1,
+                perPage: 3,
+                breakpoints: {
+                  540: {
+                    perMove: 1,
+                    perPage: 1,
+                  },
+                  768: {
+                    perMove: 2,
+                    perPage: 2,
+                  }
+                },
+              }}
+              >
+                {props.works.map((work) => (
+                  <SplideSlide key={work.id} className={styles.item}>
+                    <Link href={`/works/${work.id}`} className={styles.link}>
+                      <div className={styles.img}>
+                        <Image
+                          src={work.worksDetails.img.url}
+                          alt={work.title}
+                          width={work.worksDetails.img.width}
+                          height={work.worksDetails.img.height}
+                          layout="responsive"
+                          objectFit="cover"
+                          data-splide-lazy={work.worksDetails.img.url}
+                          priority={true}
+                        />
+                      </div>
+                      <h3 className={styles.workTitle}>{work.title}</h3>
+                    </Link>
+                </SplideSlide>
+              ))}
+            </Splide>
           </div>
-          <Splide
-            area-label="Works"
-            options={{
-              // autoplay: true,
-              // interval: 3000,
-              perPage: 1,
-              pagination: false,
-            }}
-            >
-              {props.works.map((work) => (
-                <SplideSlide key={work.id} className={styles.item}>
-                  <Link href={`/works/${work.id}`} className={styles.link}>
-                    <div className={styles.img}>
-                      <Image
-                        src={work.worksDetails.img.url}
-                        alt={work.title}
-                        width={work.worksDetails.img.width}
-                        height={work.worksDetails.img.height}
-                        layout="responsive"
-                        objectFit="cover"
-                      />
-                    </div>
-                    <h3 className={styles.workTitle}>{work.title}</h3>
-                  </Link>
-              </SplideSlide>
-            ))}
-          </Splide>
         </div>
       </section>
     </>
