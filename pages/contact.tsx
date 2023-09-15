e3ac67c97c58c69e5b2ed1cceefa79d724a050c5
Haruka Nishida <nishida1436@gmail.com>
2023-09-15 19:19:13 +0900

    お問い合わせのタイトル変更

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from '../components/layouts/header/headerSection'
 import MainSection from '../components/layouts/contact/contactForm'
 import FooterSection from '../components/layouts/footer/footerSection'
@@ -6,6 +7,9 @@ import styles from '../../../styles/contactPage.module.scss';
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Contact | Haruka Nishida Portfolio</title>
+      </Head>
       <Header />
       <MainSection />
       <FooterSection />
